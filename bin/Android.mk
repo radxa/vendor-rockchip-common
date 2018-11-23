066ededea9fffba47989349f911c296a0852aa21
Ahren <lili@vamrs.com>
2018-11-23 16:55:12 +0800

    upload parted static elf file
    Signed-off-by: Ahren <lili@vamrs.com>

--- bin/Android.mk
+++ bin/Android.mk
@@ -39,4 +39,14 @@ LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
 LOCAL_SRC_FILES := $(TARGET_ARCH)/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+###############################################################################
+# parted
+include $(CLEAR_VARS)
+LOCAL_MODULE := parted
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_STEM := $(LOCAL_MODULE)
+LOCAL_SRC_FILES := $(TARGET_ARCH)/$(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
 #endif
